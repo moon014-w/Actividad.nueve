034394cd8d06c1e2f91b08344ecdd86c0f20f145
moon014-w <orzcdrip@gmail.com>
2025-10-26 17:00:27 -0600

    Add files via upload

--- js/elementos.js
+++ js/elementos.js
@@ -1,7 +1,12 @@
 let boton = document.getElementById("guardar");
+let boton2 = document.querySelector("#guardar2");
 let nombre = document.querySelector("#nombre");
 let apellido = document.querySelector("#apellido");
 let fecha = document.querySelector("#fecha");
+let id = document.querySelector("#id");
+let buscar = document.querySelector("#buscar");
+let input_collection = document.querySelector("#collection");
+let input_id = document.querySelector("#doc");
+let resultado = document.querySelector("#resultado");
 
-
-export let datos = { boton, nombre, apellido, fecha };
+export let datos = { boton, nombre, apellido, fecha, id, boton2, buscar, input_collection, input_id, resultado };
--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
-  import { getFirestore,collection, addDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js"; 
+  import { getFirestore,collection, addDoc, setDoc, doc, getDoc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js"; 
 
   import { datos } from "./elementos.js";
   // TODO: Add SDKs for Firebase products that you want to use
@@ -48,3 +48,28 @@ datos.boton.addEventListener("click", async function() {
 } catch (e) {
   console.error("Error adding document: ", e);
 }})
+
+datos.boton2.addEventListener("click", async function() {
+  // Add a new document in collection "cities"
+  await setDoc(doc(db, "cities", datos.id.value), {
+        first: datos.nombre.value,
+        last: datos.apellido.value,
+        born: datos.fecha.value
+        
+  });
+});
+
+//leer documento//
+
+datos.buscar.addEventListener("click", async function(){});
+
+const docRef = doc(db, "datos.inputcollection.value", datos.input_id.value, "2468");
+const docSnap = await getDoc(docRef);
+
+if (docSnap.exists()) {
+  datos,resultado.textContent = `Nombre: $docSnap.data().first  Apellido: $docSnap.data().last  Fecha de Nacimiento: $docSnap.data().born`;
+  console.log("Document data:", docSnap.data());
+} else {
+  // docSnap.data() will be undefined in this case
+  console.log("No such document!");
+}
